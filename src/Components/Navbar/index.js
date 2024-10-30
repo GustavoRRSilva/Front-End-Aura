ed271023d324771e955b4513a83ea8791b7c3263
Gustavo <gstvrrds@gmail.com>
2024-10-30 19:33:40 -0300

    feat:adição do formulario

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,30 +1,50 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import "./style.scss"
-import AuraLogo from "../../Assets/auraLogo.svg"
-import MenuIcon from "../../Assets/menuIcon.svg"
-import CloseMenu from "../../Assets/closeMenu.svg"
-import { useState } from "react"
+import Image from "next/image";
+import "./style.scss";
+import AuraLogo from "../../Assets/auraLogo.svg";
+import MenuIcon from "../../Assets/menuIcon.svg";
+import CloseMenu from "../../Assets/closeMenu.svg";
+import { useState } from "react";
 export default function Navbar() {
-    const [isNavbarOpen,setIsNavbarOpen] = useState(false)
-    
-    return(
-        <header className="header">
-           <div className="content">
-            <Image className="auraLogo" src =  {AuraLogo} width={10} height={10} alt="Logo da Aura"/>
-            <nav>
-            <Image className="menuIcon" onClick={(e)=>setIsNavbarOpen(true)}  src={MenuIcon} width={10} height={10} alt="MenuIcon"/>
-            <ul className={`navbarOpen${isNavbarOpen}`}>
-                <Image onClick={(e)=>setIsNavbarOpen(false)} className="closeMenu" src = {CloseMenu} height={10} width={10} alt="Icon to close the menu"/>
-                <li>Home</li>
-                <li>Sobre</li>
-                <li>Serviços</li>
-                <li>Depoimentos</li>
-                <li>Formulário</li>
-            </ul>
-            </nav>
-           </div>
-        </header>
-    )
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="content">
+        <Image
+          className="auraLogo"
+          src={AuraLogo}
+          width={10}
+          height={10}
+          alt="Logo da Aura"
+        />
+        <nav>
+          <Image
+            className="menuIcon"
+            onClick={(e) => setIsNavbarOpen(true)}
+            src={MenuIcon}
+            width={10}
+            height={10}
+            alt="MenuIcon"
+          />
+          <ul className={`navbarOpen${isNavbarOpen}`}>
+            <Image
+              onClick={(e) => setIsNavbarOpen(false)}
+              className="closeMenu"
+              src={CloseMenu}
+              height={10}
+              width={10}
+              alt="Icon to close the menu"
+            />
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Serviços</li>
+            <li>Depoimentos</li>
+            <li>Formulário</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Navbar from "../Components/Navbar";
-import MainBanner from "../Components/MainBanner"
+import MainBanner from "../Components/MainBanner";
+import Form from "@/Components/Form";
 
 export default function Home() {
   return (
-    <div >
-       <Navbar/>
-        <div className="barBetweenContent"/> 
-       <MainBanner/>
+    <div>
+      <Navbar />
+      <div className="barBetweenContent" />
+      <MainBanner />
+      <div className="barBetweenContent" />
+      <Form />
     </div>
   );
 }
