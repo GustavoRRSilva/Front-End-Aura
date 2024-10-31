b44bc505b9254989357f4ab2812f444e5a1888c7
Williams Silva <limmwillams@gmail.com>
2024-10-31 09:11:42 -0300

    Adicionando seção de visão e missão

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import Navbar from "../Components/Navbar";
-import MainBanner from "../Components/MainBanner"
+import MainBanner from "../Components/MainBanner";
+import Missao from "../Components/Missao";
+import Visao from "../Components/Visao"; 
+
 
 export default function Home() {
   return (
-    <div >
-       <Navbar/>
-        <div className="barBetweenContent"/> 
-       <MainBanner/>
+    <div>
+      <Navbar />
+      <div className="barBetweenContent" />
+      <MainBanner />
+      <Missao /> {}
+      <Visao /> {}
     </div>
   );
 }
