de3430aae6c8e13e4f859b26191baa40d9707d60
Gustavo <gstvrrds@gmail.com>
2024-11-13 19:42:18 -0300

    feat:finalização das etapas

---
+++ src/Components/Depoimentos/index.js
@@ -0,0 +1,75 @@
+import ImageUm from "@/Assets/img-Juliana.png";
+import ImageDois from "@/Assets/img-Lucas.png";
+import ImageTres from "@/Assets/img-Mariana.png";
+import styles from "./style.module.scss";
+import Image from "next/image";
+export default function Depoimentos() {
+  const depoimentos = [
+    {
+      img: ImageUm,
+      description:
+        "A empresa realmente se destaca na implementação de soluções de IA. A equipe é super competente e suas soluções ajudaram nossa empresa a otimizar processos, aumentando nossa produtividade em 30%! Recomendo!",
+      name: "Mariana Costa",
+    },
+    {
+      img: ImageDois,
+      description:
+        "Fiquei impressionado com o suporte ao cliente. Sempre que tive dúvidas sobre a implementação da IA, a equipe foi rápida e eficaz em me ajudar. O produto é intuitivo e fácil de usar!",
+      name: "Lucas Martins",
+    },
+    {
+      img: ImageTres,
+      description:
+        "Desde que começamos a usar as soluções de IA dessa empresa, vimos uma melhoria significativa na análise de dados. As insights gerados são valiosos e têm nos ajudado a tomar decisões mais informadas.",
+      name: "Juliana Santos",
+    },
+    {
+      img: ImageDois,
+      description:
+        "Fiquei impressionado com o suporte ao cliente. Sempre que tive dúvidas sobre a implementação da IA, a equipe foi rápida e eficaz em me ajudar. O produto é intuitivo e fácil de usar!",
+      name: "Lucas Martins",
+    },
+    {
+      img: ImageTres,
+      description:
+        "Desde que começamos a usar as soluções de IA dessa empresa, vimos uma melhoria significativa na análise de dados. As insights gerados são valiosos e têm nos ajudado a tomar decisões mais informadas.",
+      name: "Juliana Santos",
+    },
+    {
+      img: ImageDois,
+      description:
+        "Fiquei impressionado com o suporte ao cliente. Sempre que tive dúvidas sobre a implementação da IA, a equipe foi rápida e eficaz em me ajudar. O produto é intuitivo e fácil de usar!",
+      name: "Lucas Martins",
+    },
+    {
+      img: ImageTres,
+      description:
+        "Desde que começamos a usar as soluções de IA dessa empresa, vimos uma melhoria significativa na análise de dados. As insights gerados são valiosos e têm nos ajudado a tomar decisões mais informadas.",
+      name: "Juliana Santos",
+    },
+  ];
+  return (
+    <section className={styles.allContent}>
+      <div className={styles.content}>
+        <h2>Depoimentos</h2>
+        <div className={styles.allDepoiments}>
+          {depoimentos &&
+            depoimentos.map((depoimento, index) => (
+              <div key={index} className={styles.depoimento}>
+                <div className={styles.imgName}>
+                  <Image
+                    width={100}
+                    height={100}
+                    src={depoimento.img}
+                    alt="person image"
+                  />
+                  <h3>{depoimento.name}</h3>
+                </div>
+                <p>{depoimento.description}</p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -6,18 +6,20 @@ import Form from "@/Components/Form";
 import Footer from "@/Components/Footer";
 import Valores from "@/Components/Valores";
 import Representantes from "@/Components/Representantes";
+import Servicos from "@/Components/Servicos";
+import Depoimentos from "@/Components/Depoimentos";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="barBetweenContent" />
       <MainBanner />
-      <Missao />
       <Visao />
+      <Missao />
       <Valores />
-      <div className="barBetweenContent" />
       <Representantes />
+      <Servicos />
+      <Depoimentos />
       <Form />
       <Footer />
     </div>
